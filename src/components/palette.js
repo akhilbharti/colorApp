3a63194dcdb0fefffcc83e54e3f37177b1fdb32e
akhil <akhbharti@gmail.com>
2020-06-12 20:52:29 +0530

    added palette and seed color

---
+++ src/components/palette.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function palette(){
+
+  return <div className="Palette">
+    {/* navbar will be added here */}
+    <div className="palette-clr">
+{/* different color boxes will go here */}
+    </div>
+    {/* eventually footer will be added here */}
+    </div>
+}
+
+
+export default palette
