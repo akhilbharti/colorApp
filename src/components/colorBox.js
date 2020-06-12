00c51eddd544a3a740fc2da5753e4cc03845a5aa
akhil <akhbharti@gmail.com>
2020-06-12 23:56:36 +0530

    copyToClipBoard package added and copy functionality to button added

--- src/components/colorBox.js
+++ src/components/colorBox.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './styles/colorBox.css'
+import {CopyToClipboard} from 'react-copy-to-clipboard'
+
 
 function ColorBox({...props}){
 const {name, background} = props
@@ -9,12 +11,13 @@ const {name, background} = props
         <div className="box-content">
           <span>{name}</span>
         </div>
+      <CopyToClipboard text={background}>
         <button className='copy-btn'>Copy</button>
+    </CopyToClipboard>
       </div>
       <span className="see-more">More</span>
     </div>
   )
 
 }
-
 export default ColorBox
