86849ce50b9fcbb86084ca134aa08e3d45845a45
akhil <akhbharti@gmail.com>
2020-07-02 02:09:32 +0530

    palettelist styles added

--- src/components/paletteList.js
+++ src/components/paletteList.js
@@ -1,13 +1,55 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import MiniPalette from './miniPalette'
+import { makeStyles } from '@material-ui/styles'
+
+const styles= makeStyles({
+  root:{
+    backgroundColor:"blue",
+    height:"100%",
+    display:"flex",
+    alignItems:"flex-start",
+    justifyContent:"center"
+    },
+  container:{
+    width:"50%",
+    display:"flex",
+    alignItems:"flex-start",
+    flexDirection:"column",
+    flexWrap:"wrap",
+  },
+  nav:{
+    display:"flex",
+    width:"100%",
+    justifyContent:"space-between",
+    color:"white"
+  },
+  palettes:{
+    boxSizing:"border-box",
+    width:"100%",
+    display:"grid",
+    gridTemplateColumns:"repeat(3, 30%)",
+    gridGap:"5%"
+  }
+})
 
 function PaletteList(props){
   const { palettes} = props
+  const classes = styles()
   const platteRoute = palettes.map(palette=>{
     return <MiniPalette {...palette}/>
   })
-return <div>{platteRoute}</div>
+return( 
+  <div className={classes.root}>
+    <div className={classes.container}>
+      <nav className={classes.nav}>
+        <h1>React Colors</h1>
+      </nav>
+      <div className={classes.palettes}>
+      {platteRoute}
+      </div>
+    </div>
+  </div>)
 }
 
 export default PaletteList
