d186cd9b63d6d77f952dea7b5788fe99c0f13e1e
akhil <akhbharti@gmail.com>
2020-07-11 04:51:08 +0530

    text color based on color luminosity

--- src/components/colorBox.js
+++ src/components/colorBox.js
@@ -1,48 +1,59 @@
-import React,{useState,useEffect} from 'react'
-import './styles/colorBox.css'
-import {CopyToClipboard} from 'react-copy-to-clipboard'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import "./styles/colorBox.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Link } from "react-router-dom";
+import chroma from "chroma-js";
 
+function ColorBox({ ...props }) {
+  const [copied, setCopied] = useState(false);
 
-
-function ColorBox({...props}){
-  const [copied, setCopied] = useState(false)
-
-  const changecopystate=()=>{
-    setCopied(true)
-  }
+  const changecopystate = () => {
+    setCopied(true);
+  };
 
   useEffect(() => {
-    if(!copied)return
-    const timer = setTimeout(() => setCopied(false), 1500)
+    if (!copied) return;
+    const timer = setTimeout(() => setCopied(false), 1500);
     return () => {
-      clearTimeout(timer)
-    }
-  }, [copied])
+      clearTimeout(timer);
+    };
+  }, [copied]);
 
-  const { name, background, id, paletteId} = props
+  const { name, background, id, paletteId } = props;
+  const isDarkColor = chroma(background).luminance() <= 0.08;
+  const isLightColor = chroma(background).luminance() >= 0.7;
+
   return (
-    <div className="color-box" style={{background}}>
-      <div style={{background}} className={`copy-overlay ${copied && 'show'}`}/>
-      <div className={`copy-msg ${copied && 'show'}`}>
+    <div className="color-box" style={{ background }}>
+      <div
+        style={{ background }}
+        className={`copy-overlay ${copied && "show"}`}
+      />
+      <div className={`copy-msg ${copied && "show"}`}>
         <h1>copied!</h1>
-        <p>{background}</p>
+        <p className={isLightColor && "dark-text"}>{background}</p>
       </div>
       <div className="copy-continer">
         <div className="box-content">
-          <span>{name}</span>
+          <span className={isDarkColor && "light-text"}>{name}</span>
         </div>
-      <CopyToClipboard text={background} onCopy={changecopystate}>
-        <button className='copy-btn'>Copy</button>
-    </CopyToClipboard>
+        <CopyToClipboard text={background} onCopy={changecopystate}>
+          <button className={`copy-btn ${isLightColor && "dark-text"}`}>
+            Copy
+          </button>
+        </CopyToClipboard>
       </div>
-      {props.showLink &&
-        <Link to={`/palette/${paletteId}/${id}`} onClick={(e) => e.stopPropagation()}>
-          <span className="see-more" >More</span>
+      {props.showLink && (
+        <Link
+          to={`/palette/${paletteId}/${id}`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <span className={`see-more ${isLightColor && "dark-text"}`}>
+            More
+          </span>
         </Link>
-      }
+      )}
     </div>
-  )
-
+  );
 }
-export default ColorBox
+export default ColorBox;
